53ca99e5b88df5d5d32d5f654380d6eaa5f2edb7
Njagi Mwaniki <njagi@urbanslug.com>
2018-04-10 00:51:34 +0300

    Fix nix build
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

---
+++ default.nix
@@ -0,0 +1,10 @@
+{ mkDerivation, base, hakyll, stdenv }:
+mkDerivation {
+  pname = "blog";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [ base hakyll ];
+  license = stdenv.lib.licenses.gpl3;
+}
